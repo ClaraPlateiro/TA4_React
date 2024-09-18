270b4f7fd44098a9509dc403736bdb52f57b685a
ClaraPlateiro <clara.plateiro@correo.ucu.edu.uy>
2024-09-18 14:03:58 -0300

    TA4 Terminado

---
+++ react-example/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+
+function Contador() {
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => {
+    setContador(contador + 1);
+  }
+
+  const decrementar = () => {
+    if (contador > 0) {
+      setContador(contador - 1);
+    }
+  }
+
+  const reiniciar = () => {
+    setContador(0);
+  }
+
+  return (
+    <div>
+      <h1>Contador de clicks: {contador}</h1>
+      <button onClick={incrementar}>Incrementar</button>
+      <button onClick={decrementar}>Decrementar</button>
+      <button onClick={reiniciar}>Reiniciar</button>
+    </div>
+  );
+}
+
+export default Contador;
